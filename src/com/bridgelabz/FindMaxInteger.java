f7d45e518611687f59de9a61bfe01b3a779ad715
priyanshusuman9 <priyanshusuman947@gmail.com>
2022-12-24 00:18:34 +0530

    commit

---
+++ src/com/bridgelabz/FindMaxInteger.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class FindMaxInteger {
+    public static Integer compareTo(Integer a,Integer b,Integer c) // three interger to compare using if - else
+    {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to find maximum no from three integer");
+        System.out.println("Max Integer Nmber is: "+FindMaxInteger.compareTo(15, 32, 45));
+    }
+
+}
